f8e5fe7d3aeaf66a54a66e9cc704621923d19e52
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-02-14 21:08:08 +0300

    Добавьте файлы проекта.

---
+++ Task16.2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16._2
+{
+    class Product
+    {
+        public int CodeProduct { get; set; }
+        public string NameProduct { get; set; }
+        public double PriceProduct { get; set; }
+    }
+}
---
+++ Task16.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Task16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr=new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+            Product costlyProduct = products[0];
+            foreach (Product p in products)
+            {
+                if (p.PriceProduct>costlyProduct.PriceProduct)
+                {
+                    costlyProduct = p;
+                }
+            }
+            Console.WriteLine($"Самый дорогой товар:{costlyProduct.NameProduct}, цена:{costlyProduct.PriceProduct}");
+            Console.ReadKey();
+        }
+    }
+}
